0183f6bf014a93b413e16770eae77a174fb3cbe6
iammarkps <markkung08@gmail.com>
2019-05-17 01:12:40 +0700

    Fixed non-audition member registration

--- app/Club.php
+++ app/Club.php
@@ -351,9 +351,8 @@ class Club extends Model {
                     case 4:
                         return floor($this->max_member * 0.35) - $this->members()->where('level', 4)->count();
                     case 5:
-                        return floor($this->max_member * 0.2) - $this->members()->where('level', '!=', 4)->count();
                     case 6:
-                        return floor($this->max_member * 0.2) - $this->members()->where('level', '!=', 4)->count();
+                        return floor($this->max_member * 0.2) - $this->members()->where('level', '!=', 4)->where('reason', '!=', User::RegisterType_ExistingMember)->count();
                 }
             }
         }
