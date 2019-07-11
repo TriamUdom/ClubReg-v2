42c898ec6d84c9d3fe7cc66eb638f14d9303ecc9
iammarkps <markkung08@gmail.com>
2019-07-11 21:43:45 +0700

    Add invalid club contact

--- resources/views/student/edit-info.blade.php
+++ resources/views/student/edit-info.blade.php
@@ -13,9 +13,13 @@
                 </select>
             </div>
         </div>
+            <div class="sector grey lighten-4 red-text" style="font-size: 1.5rem;line-height: 1.8rem">
+                หากชมรมของนักเรียนไม่ถูกต้อง โปรดติดต่อประธานชมรม หรือ Facebook Page : TUCMC ภายในวันที่ 15 กรกฎาคม 2562
+            </div>
         <button class="btn waves-effect waves-light purple fullwidth" type="submit">
             บันทึกข้อมูล
         </button>
+
         </form>
     <br/>
     <br />
